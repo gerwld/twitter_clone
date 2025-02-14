3583292d920e66e33ecb8c02b2cbf098fe311b74
P Jaworski <pjaworski.dev@gmail.com>
2025-02-14 14:32:13 +0100

    rn *.routes.js => *.route.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import authRoutes from './routes/auth.routes.js';
-import userRoutes from './routes/user.routes.js';
-import postRoutes from './routes/post.routes.js';
+import authRoutes from './routes/auth.route.js';
+import userRoutes from './routes/user.route.js';
+import postRoutes from './routes/post.route.js';
+import notificationRoutes from './routes/notification.route.js';
 import connectDB from './db/connectMongoDB.js';
 import cookieParser from 'cookie-parser';
 import { v2 as cloudinary } from 'cloudinary';
@@ -26,6 +27,7 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 app.listen(PORT, () => {
    console.log('Server is running on port ' + PORT);
